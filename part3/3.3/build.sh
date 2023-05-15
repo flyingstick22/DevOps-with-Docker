4a57baf088d7cfda754c3e98938a2d67ad7a0556
Niko <100952335+flyingstick22@users.noreply.github.com>
2023-05-15 19:27:04 +0200

    Update build.sh

--- part3/3.3/build.sh
+++ part3/3.3/build.sh
@@ -6,7 +6,7 @@ dockerhub_repo=$2
 echo "cloning GitHub repo $1"
 git clone https://github.com/$1.git
 
-echo "building docker image ..."
+echo "building docker image"
 docker build -t $dockerhub_repo
 
 docker push $dockerhub_repo
