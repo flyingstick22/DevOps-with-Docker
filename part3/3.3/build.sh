20c5519f2719b195070630eeee63e86a53f2bb64
Niko <100952335+flyingstick22@users.noreply.github.com>
2023-05-15 19:05:39 +0200

    Create build.sh

---
+++ part3/3.3/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [[ $# -ne 2 ]]; then
+    echo "Usage: $0 <github_repository> <dockerhub_repository>"
+    exit 1
+fi
+
+github_repo=$1
+dockerhub_repo=$2
+
+echo "cloning GitHub repo $1"
+git clone https://github.com/$1.git
+
+echo "building docker image ..."
+docker build -t $dockerhub_repo
+
+docker push $dockerhub_repo
